d9e910ed9be56d5886c4a541c2b978800222df06
ztp6893 <ztp6893@rit.edu>
2024-11-04 13:37:10 -0500

    Added Openings

--- covertest.py
+++ covertest.py
@@ -2,12 +2,34 @@ import chess
 import chess.pgn
 import random
 
-def random_chess_game(messages):
+# Define some common openings
+openings = {
+    "Ruy Lopez": ["e4", "e5", "Nf3", "Nc6", "Bb5"],
+    "Sicilian Defense": ["e4", "c5", "Nf3", "d6"],
+    "French Defense": ["e4", "e6", "d4", "d5"],
+    "Queen's Gambit": ["d4", "d5", "c4"],
+    "King's Indian Defense": ["d4", "Nf6", "c4", "g6"],
+}
+
+def random_chess_game(messages, opening_choice):
     board = chess.Board()
     game = chess.pgn.Game()
     node = game
 
-    # Loop until the game is over
+    # Play the selected opening
+    if opening_choice in openings:
+        opening_moves = openings[opening_choice]
+        for move_san in opening_moves:
+            move = board.parse_san(move_san)  # Parse the move in SAN notation
+            board.push(move)                  # Make the move on the board
+            node = node.add_variation(move)   # Add the move to the PGN
+
+            # Add a message if available
+            if messages:
+                comment = messages.pop(0)
+                node.comment = comment
+
+    # Continue with random moves after the opening
     while not board.is_game_over():
         # Generate a list of all legal moves
         legal_moves = list(board.legal_moves)
@@ -18,10 +40,10 @@ def random_chess_game(messages):
         # Add the move to the PGN
         node = node.add_variation(move)
 
-        # Add a message after the move if there are messages left
+        # Add a message if available
         if messages:
-            comment = messages.pop(0)  # Get and remove the first message
-            node.comment = comment     # Add the message as a comment
+            comment = messages.pop(0)
+            node.comment = comment
 
     # Set the game result
     if board.is_checkmate():
@@ -33,13 +55,13 @@ def random_chess_game(messages):
 
     return game
 
-def save_pgn(game, filename="random_chess_game_with_messages.pgn"):
+def save_pgn(game, filename="random_chess_game_with_opening.pgn"):
     # Save the PGN game to a file
     with open(filename, "w") as pgn_file:
         pgn_file.write(str(game))
 
 if __name__ == "__main__":
-    # Define a list of messages to add as comments after some moves
+    # Define a list of messages to add as comments after moves
     messages = [
         "1",
         "2",
@@ -48,8 +70,15 @@ if __name__ == "__main__":
         "5"
     ]
 
-    # Generate a random chess game with messages
-    game = random_chess_game(messages)
+    # Display available openings and let the user select one
+    print("Available openings:")
+    for i, opening in enumerate(openings.keys(), start=1):
+        print(f"{i}. {opening}")
+    choice = int(input("Select an opening by number (1-5): "))
+    opening_choice = list(openings.keys())[choice - 1]
+
+    # Generate a random chess game with the chosen opening
+    game = random_chess_game(messages, opening_choice)
     # Print the PGN to the console
     print(game)
     # Optionally save the game to a PGN file
