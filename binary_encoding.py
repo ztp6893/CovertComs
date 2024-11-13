84f4783f26844f8844f867ef98c1bf5e79ff8382
MysticalMirage <aaronschwager@comcast.net>
2024-11-12 23:58:04 -0500

    updated binary encoding to work with encoder.py

--- binary_encoding.py
+++ binary_encoding.py
@@ -1,25 +1,52 @@
 import chess
 import chess.pgn
+import random
 
-def comment_encode(board, message):
+# Define possible comments
+one_comment = [
+    "Check this out!"
+    "ha!"
+]
+
+zero_comment = [
+    "Nothing special here"
+    "boring"
+]
+
+# Opening
+french_defense = ["e4", "e6", "d4", "d5"]
+
+def comment_encode(message):
     # Convert the message into binary
     binary_message = ''.join(format(ord(char), '08b') for char in message)
 
+    board = chess.Board()
     game = chess.pgn.Game()
     node = game
+
+    for open_move in french_defense:
+        move = board.parse_san(open_move)  # Parse the move in SAN notation
+        board.push(move)                  # Make the move on the board
+        node = node.add_variation(move)   # Add the move to the PGN
+
     for bit in binary_message:
         # Make a random legal move
-        move = list(board.legal_moves)[0]
+        move = random.choice(list(board.legal_moves))
         board.push(move)
         node = node.add_variation(move)
         
         # Encode the bit as a comment
         if bit == "1":
-            node.comment = "Check this out!"  # Represents "1"
+            node.comment = random.choice(one_comment)
         else:
-            node.comment = "Nothing special here."  # Represents "0"
+            node.comment = random.choice(zero_comment)
 
     return game
+
+def comment_decode(game):
+    #tbd
+    return message
+
 
 # Example usage
 #board = chess.Board()
